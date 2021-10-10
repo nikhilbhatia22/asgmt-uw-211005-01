545b2d0596df5865b2fabf889bd89d792d7819d4
Nikhil Bhatia <nikhil@codiksh.in>
2021-10-11 02:10:55 +0530

    docs(tests): Added minor docs

--- tests/ParserExecutionTest.php
+++ tests/ParserExecutionTest.php
@@ -3,7 +3,11 @@
 
 class ParserExecutionTest extends \PHPUnit\Framework\TestCase {
 
-    public function testValidationFails() {
+    /**
+     * Test for parser bailing when validation is failed.
+     * @throws Exception
+     */
+    public function testParserBailsOnFailedValidation() {
         $this->expectException(RuntimeException::class);
         \App\Parser::execute('tests\test_products_comma_separated_invalid_data.csv', 'tests\unique-combination-results-csv.csv');
     }
