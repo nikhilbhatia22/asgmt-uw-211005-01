9c396fda95dba310a7fe40a21d7f30d62231f609
Nikhil Bhatia <nikhil@codiksh.in>
2021-10-11 01:34:10 +0530

    update(assignment): Reverted defaultings, as they were required in the file.

--- assignment.php
+++ assignment.php
@@ -7,6 +7,9 @@ $script_start_time = microtime(true);
 //Input Collection
 $inputs = getopt(null,['file:', 'unique-combination::', 'dont-bail', 'print-objects']);
 $inputFile = $inputs['file'];
+$uniqueCombinationFile = $inputs['unique-combination'] ?? 'unique-combination-results.csv';
+$bailValidation = isset($inputs['dont-bail']) ? false : true;
+$printObjects = isset($inputs['print-objects']);
 
 
 //Few Argument Validations
@@ -16,14 +19,13 @@ if(!file_exists($inputFile))   throw new InvalidArgumentException("The specified
 
 //Main Script Execution.
 try {
-    $outputFile = App\Parser::execute($inputFile, $inputs['unique-combination'], isset($inputs['dont-bail']) ? false : true,
-        isset($inputs['print-objects']));
+    App\Parser::execute($inputFile, $uniqueCombinationFile, $bailValidation, $printObjects);
 } catch (Exception $e) {
     echo "Some exception $e was raised";
 }
 
 
 $script_end_time = microtime(true);
-echo "The results were successfully parsed into $outputFile in "
+echo "The results were successfully parsed into $uniqueCombinationFile in "
     . round(($script_end_time - $script_start_time), 2) . ' seconds with peak memory usage of '
     . \App\Memory::formatBytes(memory_get_peak_usage());
