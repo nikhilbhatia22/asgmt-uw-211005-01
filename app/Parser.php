ba23dfb87ccab8a575aad9909eae178dfaa7bcc2
Nikhil Bhatia <nikhil@codiksh.in>
2021-10-11 00:40:55 +0530

    update(parser): Added default values to the function directly instead of manually setting

--- app/Parser.php
+++ app/Parser.php
@@ -12,9 +12,11 @@ class Parser {
      * @param $uniqueCombinationFile
      * @param $bailValidation
      * @param $printObjects
+     * @return string
      * @throws \Exception
      */
-    public static function execute($inputFile, $uniqueCombinationFile, $bailValidation, $printObjects){
+    public static function execute($inputFile, $uniqueCombinationFile = 'unique-combination-results.csv',
+                                   $bailValidation = true, $printObjects = false){
         switch (pathinfo($inputFile, PATHINFO_EXTENSION)) {
             case 'json': echo "Parsing for JSON is planned and shall be done soon."; exit; break;
             case 'XML': echo "Parsing for XML is planned and shall be done soon."; exit; break;
@@ -24,6 +26,7 @@ class Parser {
                 CsvParser::execute($inputFile, $uniqueCombinationFile, $bailValidation, $printObjects);
                 break;
         }
+        return $uniqueCombinationFile;
     }
 
 }
--- assignment.php
+++ assignment.php
@@ -7,9 +7,6 @@ $script_start_time = microtime(true);
 //Input Collection
 $inputs = getopt(null,['file:', 'unique-combination::', 'dont-bail', 'print-objects']);
 $inputFile = $inputs['file'];
-$uniqueCombinationFile = $inputs['unique-combination'] ?? 'unique-combination-results.csv';
-$bailValidation = isset($inputs['dont-bail']) ? false : true;
-$printObjects = isset($inputs['print-objects']);
 
 
 //Few Argument Validations
@@ -19,13 +16,14 @@ if(!file_exists($inputFile))   throw new InvalidArgumentException("The specified
 
 //Main Script Execution.
 try {
-    App\Parser::execute($inputFile, $uniqueCombinationFile, $bailValidation, $printObjects);
+    $outputFile = App\Parser::execute($inputFile, $inputs['unique-combination'], isset($inputs['dont-bail']) ? false : true,
+        isset($inputs['print-objects']));
 } catch (Exception $e) {
     echo "Some exception $e was raised";
 }
 
 
 $script_end_time = microtime(true);
-echo "The results were successfully parsed into $uniqueCombinationFile in "
+echo "The results were successfully parsed into $outputFile in "
     . round(($script_end_time - $script_start_time), 2) . ' seconds with peak memory usage of '
     . \App\Memory::formatBytes(memory_get_peak_usage());
